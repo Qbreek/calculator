93897535ef3fca59798f33e326609879bd5a8808
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-02-25 22:07:19 +0200

    Works with multiple numbers and multiple types of operands. Executes the operations following the canonical order : from left to right , divisions and multiplications first then the rest of the operations. Last step will be to add the parenthesis detection

--- script.js
+++ script.js
@@ -1,102 +1,77 @@
 let currentNumber = '';
 let computeStorage = [];
-const screen = document.querySelector('.screen');
+const operationScreen = document.querySelector('.operation');
+const answerScreen = document.querySelector('.answer');
 
 /*  Add an event listener to each number of the calculator.
     For every number clicked update the display. Store the-
     user input into the variable currentNumber.
 */
-
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         
         currentNumber += number.value;
-        screen.textContent += number.value;
+        operationScreen.textContent += number.value;
     });
 });
 
 /*  Add an event listener to each operand of the calculator.
     If the currentNumber variable is empty, the current inp-
     put was empty, so 0 is assigned. Store 0 to computeStor-
-    age.If the currentNumber != empty convert to Number and-
-    store into computeStorage.Update the display accordingly
+    age. If the currentNumber != empty convert to Number and
+    store into computeStorage. Update the display.
 */
-
 const operands = document.querySelectorAll('.operand');
 operands.forEach(operand => {
     operand.addEventListener('click', () => {
        
-        if(currentNumber === '') {
+        if( currentNumber === '' ) {
             
-            screen.textContent += 0 + operand.value;
+            operationScreen.textContent += 0 + operand.value;
             computeStorage.push(0);
             computeStorage.push(operand.value);
-            console.log(computeStorage);
 
-        }else {
+        } else {
             
+            operationScreen.textContent += operand.value;
             computeStorage.push(Number(currentNumber));
             computeStorage.push(operand.value);
-            console.log(computeStorage)
-            screen.textContent += operand.value;
             currentNumber = '';
 
         };
     });
 });
 
-let firstNumber,secondNumber;
 
 let result;
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
    
-    if(currentNumber === '') {
+    if( currentNumber === '' ) {
         
-        screen.textContent += 0 + '=';
+        operationScreen.textContent += 0
         computeStorage.push(0);
 
-    }else {
+    } else {
        
         computeStorage.push(Number(currentNumber));
-        screen.textContent += '=';
         console.log(computeStorage);
 
     };
 
-    i = 0;
+    let i = 0;
 
     while ( computeStorage.length != 1 ) {
         
-        if ( computeStorage.includes('*') || computeStorage.includes('/') ) {
+        indexOfMult = computeStorage.indexOf('*');
+        indexOfDiv = computeStorage.indexOf('/');
 
-            indexOfMult = computeStorage.indexOf('*');
-            indexOfDiv = computeStorage.indexOf('/');
-            console.log(indexOfDiv);
-            console.log(indexOfMult);
-            
-            if ( (indexOfMult < indexOfDiv) && (indexOfMult != -1) ) {
-                
-                i = computeStorage.indexOf('*');
-                console.log(i);
-                result = computeStorage[i-1] * computeStorage[i+1];
-                computeStorage.splice(i-1 , 3, result);
-                console.log(computeStorage);
-                i = 0;
-    
-            } else if ( indexOfMult > indexOfDiv && indexOfDiv != -1 && indexOfMult != -1) {
-               
-                i = computeStorage.indexOf('/');
-                console.log(i);
-                result = computeStorage[i-1] / computeStorage[i+1];
-                computeStorage.splice(i-1 , 3, result);
-                console.log(computeStorage); 
-                i = 0;
-            }
-
-        } else {
+        /*  No division or multiplication found in compute-
+            Storage. Add from left to right. 
+        */
+        if (indexOfMult === -1 && indexOfDiv === -1) {
             
             if ( computeStorage[i+1] === '+' ) {
                 
@@ -104,38 +79,63 @@ equals.addEventListener('click', () => {
                 computeStorage.splice(i, 3, result);
                 console.log(computeStorage);
     
-            }else if ( computeStorage[i+1] === '-') {
+            } else if ( computeStorage[i+1] === '-') {
                 
                 result = computeStorage[i] - computeStorage[i+2];
                 computeStorage.splice(i, 3, result);
                 console.log(computeStorage);
             
-            }else if ( computeStorage[i+1] === '*') {
-    
-                result = computeStorage[i] * computeStorage[i+2];
-                computeStorage.splice(i, 3, result);
-                console.log(computeStorage);        
-            
-            }else if ( computeStorage[i+1] === '/') {
-    
-                if ( computeStorage[i+2] === 0 ) {
-                    
-                    console.log('cant divide with zero');
-                    break;
-            
-                }else {
-                    
-                    result = computeStorage[i] / computeStorage[i+2];
-                    computeStorage.splice(i, 3, result);
+            };
+        
+        // Multiplication
+        } else {
+
+            // case with only /.
+            if ( indexOfMult === -1 ) {
+
+                if ( indexOfDiv != -1 ) {
+
+                    i = indexOfDiv;
+                    result = computeStorage[i-1] / computeStorage[i+1];
+                    computeStorage.splice(i-1 , 3, result);
                     console.log(computeStorage);
-                    
+                
+                };
+            
+            // case with only *.
+            } else if (indexOfDiv === -1 ) {
+
+                if ( indexOfMult != -1 ) {
+                    i = indexOfMult;
+                    result = computeStorage[i-1] * computeStorage[i+1];
+                    computeStorage.splice(i-1 , 3, result);
+                    console.log(computeStorage);
+                
+                };
+            
+            // case with both div and mult
+            } else {
+
+                if ( indexOfMult < indexOfDiv ) {
+
+                    i = indexOfMult;
+                    result = computeStorage[i-1] * computeStorage[i+1];
+                    computeStorage.splice(i-1 , 3, result);
+                    console.log(computeStorage);
+
+                } else {
+
+                    i = indexOfDiv;
+                    result = computeStorage[i-1] / computeStorage[i+1];
+                    computeStorage.splice(i-1 , 3, result);
+                    console.log(computeStorage);
+
                 };
             };
-        };  
+        };
+        
+        i = 0;
+        console.log('ends');
     };
-
-    console.log('ends');
-
+    answerScreen.textContent = computeStorage[0];
 });
-
-
