613e6ee0a1e1534dcf28a29be95886f81cf30378
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-02 14:06:06 +0200

    Major code refactoring. DEL button works as intended, chaining operands without numbers in between will lead to an error. Need to implement multiplication and division with negative numbers

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import updateDisplay from "./modules/updateDisplay.js"
 import clearDisplay from "./modules/clearDisplay.js";
 import initializeVariables from "./modules/initializeVariables.js";
 import appendToStorage from "./modules/appendToStorage.js";
+import disableBtn from "./modules/disableBtn.js";
+import enableBtn from "./modules/enableBtn.js";
 
 function main () {
     
@@ -17,32 +19,32 @@ function main () {
         [answer, currentNumber, computeStorage] = initializeVariables();
         clearDisplay(operationScreen);
         clearDisplay(answerScreen);
+        enableBtn(deleteBtn, equalsBtn);
 
     });
 
-    // TODO
+    // Delete numbers and expressions on display AND on computeStorage.
     const deleteBtn = document.querySelector('.delete');
     deleteBtn.addEventListener('click', () => {
 
-        operationScreen.textContent = operationScreen.textContent.slice( 0, operationScreen.textContent.length - 1 );
+        operationScreen.textContent = operationScreen.textContent.slice( 0, operationScreen.textContent.length - 1 );    // Slice the input on display.
         
         if ( currentNumber != '' ) {
             
-            currentNumber = currentNumber.slice(0, currentNumber.length-1);
-            console.log(currentNumber);
+            currentNumber = currentNumber.slice(0, currentNumber.length-1);    // Slice the current number on display.
         
         } else {
 
-            if ( typeof (computeStorage[computeStorage.length - 1]) === 'number' ) {
+            // Current number is currently empty. Check what was last added to the buffer.
+            if ( typeof (computeStorage[computeStorage.length - 1]) === 'number' ) {    // If its a number, this becomes the current number.
 
                 currentNumber = String(computeStorage[computeStorage.length-1]);
-                console.log(currentNumber);
+                currentNumber = currentNumber.slice(0, currentNumber.length-1);    
+                computeStorage.pop();    // Pop the last number from computer storage.
 
-            } else {
+            } else {    // If it's an operand , pop the operand from the buffer.
                 
                 computeStorage.pop();
-                console.log(computeStorage);
-                console.log('operand');
 
             };
         };
@@ -65,38 +67,27 @@ function main () {
     });
     /*  
         Add an event listener to each operand of the calculator.
-        If the currentNumber variable is empty, the current inp-
-        put was empty, so 0 is assigned. Store 0 to computeStor-
-        age. If the currentNumber != empty convert to Number and
-        store into computeStorage. Update the display.
+        Convert currentNumber from String to Number and store i-
+        -nto computeStorage. Update the display.
     */
     const operandBtn = document.querySelectorAll('.operand');
     operandBtn.forEach(operand => {
         operand.addEventListener('click', () => {
+
+            enableBtn(deleteBtn, equalsBtn);
         
-            if ( currentNumber === '' && answer === null ) {
+            if ( currentNumber === '') {    // If the user chains operands without a number in between, this will lead to an error.
                 
                 updateDisplay(operationScreen, operand.value);
-                appendToStorage(computeStorage, Number(currentNumber), operand.value);
-                /*
-                updateDisplay(operationScreen, 0 + operand.value);
-                appendToStorage(computeStorage, 0, operand.value);
-                */
+                computeStorage.push(operand.value);
 
-            } else if ( answer === null ) {
+            } else if ( answer === null ) {    // Based on the value of the var answer the displays are updated correctly.
                 
                 updateDisplay(operationScreen, operand.value);
                 appendToStorage(computeStorage, Number(currentNumber), operand.value);
                 currentNumber = '';
 
-            } else if ( currentNumber === '' && answer != null ) {
-                
-                clearDisplay(operationScreen);
-                updateDisplay(operationScreen, 0 + operand.value);
-                appendToStorage(computeStorage, 0, operand.value);
-                answer = null;
-
-            } else if ( currentNumber != '' && answer != null ){
+            } else  if ( answer != null ) {
 
                 clearDisplay(operationScreen);
                 updateDisplay(operationScreen, answer + operand.value);
@@ -110,18 +101,18 @@ function main () {
 
     const equalsBtn = document.querySelector('.equals');
     equalsBtn.addEventListener('click', () => {
-        
-        console.log(computeStorage);
-   
-        if (currentNumber != '') {
 
-            computeStorage.push(Number(currentNumber));
+        disableBtn(deleteBtn, equalsBtn);
+   
+        if (currentNumber === '') {
+
+            computeStorage.pop();
 
         } else {
 
-            computeStorage.pop();
-            console.log(computeStorage);
-        }
+            computeStorage.push(Number(currentNumber));
+
+        };
 
         answer = compute(computeStorage);
         currentNumber = answer;
