4634299695d9823a34fb47c29f4f7b2cc2e1be7f
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-04 02:07:16 +0200

    Refactoring

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import enableBtn from "./modules/enableBtn.js";
 
 function main () {
     
-    let [answer, currentNumber, computeStorage, operationHistoryCount] = initializeVariables();
+    let [answer, currentUserInput, computeStorage, operationHistoryCount] = initializeVariables();
     const operationScreen = document.querySelector('.operation');    // Displays the operation querry.
     const answerScreen = document.querySelector('.answer');    // Displays the answer.
     const operationHistory = document.querySelector('.operation-history');
@@ -17,7 +17,7 @@ function main () {
     const allClearBtn = document.querySelector('.all-clear');
     allClearBtn.addEventListener('click', () => {
         
-        [answer, currentNumber, computeStorage] = initializeVariables();
+        [answer, currentUserInput, computeStorage] = initializeVariables();
         clearDisplay(operationScreen);
         clearDisplay(answerScreen);
         enableBtn(deleteBtn, equalsBtn);
@@ -29,17 +29,16 @@ function main () {
 
         operationScreen.textContent = operationScreen.textContent.slice(0, operationScreen.textContent.length - 1);    // Slice the input on display.
         
-        if (currentNumber != '') {
+        if (currentUserInput != '') {
             
-            currentNumber = currentNumber.slice(0, currentNumber.length-1);    // Slice the current number variable.
+            currentUserInput = currentUserInput.slice(0, currentUserInput.length-1);    // Slice the current number variable.
         
-        } else {
-
-            // Current number is currently empty. Check what was last added to the buffer.
+        } else {    // Current number is currently empty. Check what was last added to the buffer.
+            
             if (typeof (computeStorage[computeStorage.length - 1]) === 'number') {    // If its a number, this becomes the current number.
 
-                currentNumber = String(computeStorage[computeStorage.length-1]);
-                currentNumber = currentNumber.slice(0, currentNumber.length-1);    
+                currentUserInput = String(computeStorage[computeStorage.length-1]);
+                currentUserInput = currentUserInput.slice(0, currentUserInput.length-1);    
                 computeStorage.pop();    // Pop the last number from computer storage.
 
             } else {    // If it's an operand , pop the operand from the buffer.
@@ -52,20 +51,20 @@ function main () {
     /*  
         Add an event listener to each number of the calculator.
         For every number clicked update the display. Store the-
-        user input into the variable currentNumber.
+        user input into the variable currentUserInput.
     */
     const numberBtn = document.querySelectorAll('.number');
     numberBtn.forEach(number => {
         number.addEventListener('click', () => {
-            
-            currentNumber += number.value;
+
+            currentUserInput += number.value;
             updateDisplay(operationScreen, number.value);
         });
     });
 
     /*  
         Add an event listener to each operand of the calculator.
-        Convert currentNumber from String to Number and store i-
+        Convert currentUserInput from String to Number and store i-
         -nto computeStorage. Update the display.
     */
     const operandBtn = document.querySelectorAll('.operand');
@@ -74,7 +73,7 @@ function main () {
 
             enableBtn(deleteBtn, equalsBtn);
         
-            if (currentNumber === '') {    // If the user chains operands without a number in between, this will lead to an error.
+            if (currentUserInput === '') {    // If the user chains operands without a number in between, this will lead to an error.
                 
                 updateDisplay(operationScreen, operand.value);
                 computeStorage.push(operand.value);
@@ -82,15 +81,15 @@ function main () {
             } else if (answer === null) {    // Based on the value of the var answer the displays are updated correctly.
                 
                 updateDisplay(operationScreen, operand.value);
-                appendToStorage(computeStorage, Number(currentNumber), operand.value);
-                currentNumber = '';
+                appendToStorage(computeStorage, Number(currentUserInput), operand.value);
+                currentUserInput = '';
 
             } else  if (answer != null) {
 
                 clearDisplay(operationScreen);
                 updateDisplay(operationScreen, answer + operand.value);
-                appendToStorage(computeStorage, Number(currentNumber), operand.value);
-                currentNumber = '';
+                appendToStorage(computeStorage, Number(currentUserInput), operand.value);
+                currentUserInput = '';
                 answer = null;
             };
         });
@@ -101,17 +100,17 @@ function main () {
 
         disableBtn(deleteBtn, equalsBtn);
         
-        if (currentNumber === '' && computeStorage[computeStorage.length - 1] != ')') {
+        if (currentUserInput === '' && computeStorage[computeStorage.length - 1] != ')') {
             
             computeStorage.pop();
             
         } else {
             
-            computeStorage.push(Number(currentNumber));
+            computeStorage.push(Number(currentUserInput));
         };
         
         answer = compute(computeStorage);
-        currentNumber = answer;
+        currentUserInput = answer;
         computeStorage = [];
 
         // make this a function
@@ -123,18 +122,17 @@ function main () {
         
         if (answerScreen.textContent = '') {
             
-            updateDisplay(answerScreen, currentNumber);
+            updateDisplay(answerScreen, currentUserInput);
             
         } else {
             
             clearDisplay(answerScreen);
-            updateDisplay(answerScreen, currentNumber);
+            updateDisplay(answerScreen, currentUserInput);
         };
     });
 };
 
 let test = [56, '+', '(', 32, '*', 9, ')', '+', '(', 23, '-', 5, ')'];
 console.log(compute(test));
-
 
 main();
